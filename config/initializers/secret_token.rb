95f816cb545fa2a0d38309dcb06af28b54031399
Bèr Kessels <ber@webschuur.com>
2012-09-23 16:14:01 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campernotities::Application.config.secret_token = '686eb08b780f754a5526deedbdcac8618078c4c180ebf356f424a30382ec7164668c69598f997ef2d7c77b730005b4de94b06a2ee258e10f0aa63e996ffd242c'
