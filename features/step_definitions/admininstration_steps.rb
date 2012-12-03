b1165bb3cc2fcec2a71fdc054532ef52487ba295
Bèr Kessels <ber@webschuur.com>
2012-12-03 15:38:45 +0100

    Introduce ActiveAdmin.
    * Replaces User with AdminUser.
    * Renders all custom User stuff obsolete; requires cleanup still!
    * Database needs rebuild from scratch.
    
    Signed-off-by: Bèr Kessels <ber@webschuur.com>

---
+++ features/step_definitions/admininstration_steps.rb
@@ -0,0 +1,7 @@
+When /^I vist the administration dashboard$/ do
+  visit "/admin"
+end
+
+Then /^I should see a dashboard$/ do
+  pending # express the regexp above with the code you wish you had
+end
