9ad7fedbba1521a6f59fb29cf0cafe39cf94536f
Bèr Kessels <ber@webschuur.com>
2012-12-03 22:59:15 +0100

    Camping Admin controller makes sure author is "current_admin_user"
    By overriding the controller action "create"
    And form needs override, else it includes a dropdown for "author"

--- app/admin/camping.rb
+++ app/admin/camping.rb
@@ -1,1 +1,19 @@
-ActiveAdmin.register Camping
+ActiveAdmin.register Camping do
+
+  form do |f|
+    f.inputs "Camping" do
+      f.input :name
+      f.input :description
+    end
+
+    f.buttons
+  end
+
+  controller do
+    def create
+      @camping = Camping.new(params[:camping])
+      @camping.author = current_admin_user
+      create!
+    end
+  end
+end
