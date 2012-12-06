6f5bcb40b7e16453582bfe273409e6578a6bd6b4
Bèr Kessels <ber@webschuur.com>
2012-12-06 15:54:53 +0100

    Add title to profile page.

--- features/step_definitions/authors_steps.rb
+++ features/step_definitions/authors_steps.rb
@@ -1,7 +1,16 @@
 Given /^there is an author "(.*?)"$/ do |name|
-  @author = AdminUser.new(:name  => name)
+  AdminUser.new(
+    :name  => name,
+    :email => "harry.potter@hogwards.edu.uk",
+    :password => "password", :password_confirmation => "password"
+  ).save!
 end
 
-When /^I visit the profile for "(.*?)"$/ do |arg1|
-  
+When /^I visit the profile for "(.*?)"$/ do |name|
+  @author = AdminUser.where(:name => name).first
+  visit author_path @author
+end
+
+Then /^I should see the title "(.*?)"$/ do |title|
+  page.find("h1.title").should have_content(title)
 end
