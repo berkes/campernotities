e9d952b0b26bc2dccd2ce6f1392a5e6c61aa2f29
Bèr Kessels <ber@webschuur.com>
2013-01-17 18:09:32 +0100

    Refactor factories and fixtures: move into spec/fixtures

--- spec/fixtures/factories.rb
+++ spec/fixtures/factories.rb
@@ -9,6 +9,9 @@ FactoryGirl.define do
   factory :camping do |f|
     f.name "Beautifull Green"
     f.association :author, :factory => :admin_user
+    f.images {
+      [FactoryGirl.create(:image)]
+    }
   end
 
   factory :admin_user, :aliases => [:author, :user] do |f|
@@ -16,6 +19,10 @@ FactoryGirl.define do
     f.email
     f.password "password"
     f.password_confirmation "password"
+  end
+
+  factory :image do |f|
+    f.image File.open(File.join(File.expand_path(File.dirname(__FILE__)), "images", "brown_small.jpg"))
   end
 end
 
