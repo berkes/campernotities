739cdceb215094b3cd10cca9bd4775e45522a01b
Bèr Kessels <ber@webschuur.com>
2013-01-19 17:11:49 +0100

    Validate image attachment

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,8 @@ class Image < ActiveRecord::Base
 
   # Paperclip
   has_attached_file :image
+  validates :image, 
+    :presence => :true,
+    :attachment_content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png"] },
+    :attachment_size => { :in => 0..5.megabytes }
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,4 +1,11 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Image do
+  it { should have_attached_file(:image) }
+  it { should validate_attachment_presence(:image) }
+  it { should validate_attachment_content_type(:image).
+    allowing('image/png', 'image/jpg', 'image/jpeg').
+    rejecting('text/plain', 'text/xml', 'image/gif') }
+  it { should validate_attachment_size(:image).
+    less_than(5.megabytes) }
 end
