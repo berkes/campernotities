2f86ba77d94d5614eff538a171b4baa0f748248d
Bèr Kessels <ber@webschuur.com>
2013-03-12 15:03:31 +0100

    refactor maps.js to DRY"

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -18,7 +18,7 @@ map.setup = function () {
       return null;
     }
     else {
-      var position = {"coords" : { "latitude": latitude, "longitude": longitude }};
+      var position = {coords: { latitude: latitude, longitude: longitude }};
       map.setCenter(position);
     }
   }
@@ -38,13 +38,13 @@ map.init = function (position) {
 
   var mapOptions = {
     zoom: 9,
-    center: new google.maps.LatLng(map.center.latitude, map.center.longitude),
+    center: map.center,
     mapTypeId: google.maps.MapTypeId.HYBRID
   }
   map.gmap = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
   new google.maps.Marker({
-    position: new google.maps.LatLng(map.center.latitude, map.center.longitude),
+    position: map.center,
     map: map.gmap,
     title: "You are here!",
     icon: new google.maps.MarkerImage("https://chart.googleapis.com/chart?chst=d_map_pin_icon_withshadow&chld=home|FFFF00")
@@ -52,17 +52,17 @@ map.init = function (position) {
 
   map.markers.forEach (function (marker){
     new google.maps.Marker({
-      position: new google.maps.LatLng(marker.latitude, marker.longitude),
-      map: map.gmap,
-      title: marker.title
+      position: marker.coords,
+      map:      map.gmap,
+      title:    marker.title
     });
   });
 }
 
 map.addMarker = function (latitude, longitude, title) {
-  map.markers.unshift({latitude: latitude, longitude: longitude, title: title});
+  map.markers.unshift({coords: new google.maps.LatLng(latitude, longitude), title: title});
 }
 
 map.setCenter = function (position) {
-  map.center = {latitude: position.coords.latitude, longitude: position.coords.longitude}
+  map.center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 }
