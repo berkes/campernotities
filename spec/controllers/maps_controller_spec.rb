8588bf9b705676ddb12015a4bd5c2c2e937873e4
Bèr Kessels <ber@webschuur.com>
2013-04-23 17:39:45 +0200

    Condense spec with shoulda syntax

--- spec/controllers/maps_controller_spec.rb
+++ spec/controllers/maps_controller_spec.rb
@@ -6,26 +6,7 @@ describe MapsController do
       get :index
     end
     it { should assign_to(:title) }
-
-    it "returns http success" do
-      response.should be_success
-    end
-
-    it "fetches only geocoded campings" do
-      campings = mock
-      campings.stub(:top).and_return(campings)
-      Camping.should_receive(:geocoded).and_return(campings)
-      get :index
-    end
-
-    it "fetches the 50 latest campings" do
-      Camping.should_receive(:top).with(50)
-      get :index
-    end
-
-    it "assign the campings to pins" do
-      expect(assigns(:pins)).to eq(Camping.top(50).geocoded)
-    end
+    it { should respond_with(:success) }
+    it { should assign_to(:pins).with(Camping.top(50).geocoded) }
   end
-
 end
