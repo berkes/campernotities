1b1677a3f23d720c449f641a1e780b32e02802da
Bèr Kessels <ber@webschuur.com>
2013-04-24 10:36:43 +0200

    use geocoded queries.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,15 @@
 class MapsController < ApplicationController
   def index
-    @pins  = Camping.geocoded.top(50)
+    if params[:bounding].nil?
+      @pins = Camping.geocoded.top(50)
+    else
+      box = params[:bounding].split(',').map{|bearing| bearing.to_f}
+      @pins = Camping.within_bounding_box(box).top(50)
+    end
     @title = "Discover Campings by Location"
+    respond_to do |format|
+      format.html
+      format.json { render :json => @pins }
+    end
   end
 end
--- spec/controllers/maps_controller_spec.rb
+++ spec/controllers/maps_controller_spec.rb
@@ -2,11 +2,36 @@ require 'spec_helper'
 
 describe MapsController do
   describe "GET 'index'" do
+    before :all do
+      @campings = FactoryGirl.create_list(:camping_with_geo, 5)
+    end
+
     before do
       get :index
     end
     it { should assign_to(:title) }
     it { should respond_with(:success) }
     it { should assign_to(:pins).with(Camping.top(50).geocoded) }
+
+    context "JSON" do
+      before do
+        get :index, :format => :json
+      end
+      it { should respond_with(:success) }
+      it 'should have the pins as JSON' do
+        response.body.should eq Camping.top(50).geocoded.to_json
+      end
+    end
+
+    context "bounded" do
+      before do
+        get :index, :bounding => "51.332168,4.468309,52.350285,7.214891"
+      end
+      it { should respond_with(:success) }
+      it "should assign @pins with items within a bounding box" do
+        campings = Camping.within_bounding_box([51.332168, 4.468309, 52.350285, 7.214891]).top(50)
+        should assign_to(:pins).with(campings)
+      end
+    end
   end
 end
