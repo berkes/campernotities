d0c43bb7da172f079f31d455e3106be81de0912c
Bèr Kessels <ber@webschuur.com>
2013-05-01 14:19:43 +0200

    Point to deploy on monica

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,14 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "CN_campernotities"
+set :repository,  "https://github.com/berkes/campernotities"
+
+set :deploy_to, "/var/www/#{application}"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "MNC_monica"                          # Your HTTP server, Apache/etc
+role :app, "MNC_monica"                          # This may be the same as your `Web` server
+role :db,  "MNC_monica", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -15,11 +16,15 @@ role :db,  "your slave db-server here"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
+# Gnome-terminal or Ubuntu or whatever trows errors. No idea what 
+# this does, but it fixes the issue. :D
+default_run_options[:pty] = true
+
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
